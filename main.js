beca5f69695a3f29568c2cecd02535df1ca4a3a6
Teekay <tkmaile96@gmail.com>
2025-06-12 13:22:08 +0200

    Equal button event handler

--- main.js
+++ main.js
@@ -24,3 +24,28 @@ function operatorBtnPressed(operator){
     value1 = parseInt(document.getElementById("input-box").value);
     newLine = true;
 }
+
+// Equals button event handler
+function equalBtnPressed(){
+    var value2 = parseInt(document.getElementById("input-box").value);
+    var result;
+    
+    switch(currentOperator) {
+        case "+":
+            result = value1 + value2;
+            break;
+        case "-":
+            result = value1 - value2;
+            break;
+        case "x":
+            result = value1 * value2;
+            break;
+        case "/":
+            result = value1 / value2;
+
+    }
+    document.getElementById("input-box").value = result;
+
+    newLine = true;
+    value1 = 0;
+}
